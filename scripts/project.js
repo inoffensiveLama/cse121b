fd8765aebee7736d38c1c3624d32954d2b91df6f
janZ <janzander96@gmail.com>
2023-10-20 15:24:42 +0800

    finished the project page.

---
+++ scripts/project.js
@@ -0,0 +1,84 @@
+const characterElement = document.querySelector("#characters");
+var characterList = [];
+
+/* async displayCharacters Function */
+const displayCharacters = (characters) => {
+    characters.forEach(character => {
+        const articleElement = document.createElement("article");
+
+        const h3Element = document.createElement("h3");
+        h3Element.textContent = character.name;
+
+        const heightElement = document.createElement("p");
+        heightElement.textContent = "Heigth: " + character.height;
+
+        const weightElement = document.createElement("p");
+        weightElement.textContent = "Weight: " + character.mass;
+
+        const genderElement = document.createElement("p");
+        genderElement.textContent = "Gender: " + character.gender;
+
+        const hairElement = document.createElement("p");
+        hairElement.textContent = "Hair Color: " + character.hair_color;
+
+        const birthElement = document.createElement("p");
+        birthElement.textContent = "Birth Year: " + character.birth_year;
+        
+        articleElement.appendChild(h3Element);
+        articleElement.appendChild(heightElement);
+        articleElement.appendChild(weightElement);
+        articleElement.appendChild(genderElement);
+        articleElement.appendChild(hairElement);
+        articleElement.appendChild(birthElement);
+
+        characterElement.appendChild(articleElement);
+    });
+}
+
+/* async getCharacters Function using fetch()*/
+const getCharacters = async () => {
+    try {
+        const response = await fetch("https://swapi.dev/api/people");
+        const data = await response.json();
+
+        if (data.results && Array.isArray(data.results)) {
+            characterList = data.results;
+            displayCharacters(characterList);
+        } else {
+            console.error("Invalid data structure received from the API.");
+        }
+    } catch (error) {
+        console.error("Error fetching character data:", error);
+    }
+}
+
+/* reset Function */
+function reset(elementID)
+{
+    document.getElementById(elementID).innerHTML = "";
+}
+
+
+/* sortBy Function */
+function sortBy(characters){
+    reset("characters");
+    let filter = document.getElementById("sortBy").value;
+    switch (filter){
+        case "male":
+            displayCharacters(characters.filter(character => character.gender === "male"));
+            break;
+        case "female":
+            displayCharacters(characters.filter(character => character.gender === "female"));
+            break;
+        case "all":
+            displayCharacters(characters);
+            break;
+        default:
+            break;
+    }
+}
+
+/* Event Listener */
+document.querySelector("#sortBy").addEventListener("change", () => { sortBy(characterList) });
+
+getCharacters();
